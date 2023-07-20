8361b48b81515400325cba02c937aca30e4e89b4
Artem Dudnik <schartoym@gmail.com>
2023-07-20 18:31:52 +0200

    Перенес переменные из inventory в отдельные файлы

--- terraform/inventory.tf
+++ terraform/inventory.tf
@@ -1,5 +1,5 @@
 resource "local_file" "ansible_inventory" {
-  content = templatefile("inventory.tmpl", {
+  content = templatefile("templates/inventory.tmpl", {
     instances = [
         yandex_compute_instance.vm-1,
         yandex_compute_instance.vm-2
@@ -12,4 +12,27 @@ resource "local_file" "ansible_inventory" {
     domain_name: var.domain_name
   })
   filename = "../inventory.ini"
+}
+
+resource "local_file" "ansible_all_vars" {
+  content = templatefile("templates/all-vars.tmpl", {
+    db = {
+      name: var.database_name,
+      user: var.database_user,
+      host: local.database_host
+    }
+    domain_name: var.domain_name
+  })
+  filename = "../group_vars/all/all.generated.yml"
+}
+
+resource "local_file" "ansible_db_vars" {
+  content = templatefile("templates/db-vars.tmpl", {
+    db = {
+      name: var.database_name,
+      user: var.database_user,
+      host: local.database_host
+    }
+  })
+  filename = "../group_vars/webservers/db.generated.yml"
 }
